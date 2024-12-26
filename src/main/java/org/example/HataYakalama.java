d97f78c3e9568d52696a8691637a70b179009353
Kaan Civelek <businesskaancivelek@gmail.com>
2024-12-26 16:23:12 +0300

    Girilecek integer, double değerler için ve seçim aralığı içindeki değer için kapsam taşması ve veri tipi tutarsızlığını ele alan özel Scanner oluşturdum.

--- src/main/java/org/example/HataYakalama.java
+++ src/main/java/org/example/HataYakalama.java
@@ -1,4 +1,62 @@
 package org.example;
-
+import java.util.Scanner;
 public class HataYakalama {
+
+    /* Hatalari ele almak icin tanimladigim ozel scanner sinifi.
+    Calisma prensibi girdiyi string olarak alıp onu istenen tipte ki degere cevirmeye calismak basaramazsa
+    uyari mesaji atıp tekrar deger girilmesini istiyor.
+     */
+
+
+     class SafeScanner {
+
+        Scanner scanner = new Scanner(System.in);
+     SafeScanner() {}
+
+        // Integer Degerler icin.
+        int nextInt() {
+            boolean gecerlilik = false;
+            String girdi;
+            int cikti = 0;
+
+            while (!gecerlilik) {
+                girdi = scanner.nextLine();
+                try {
+                    cikti = Integer.parseInt(girdi);
+                    gecerlilik = true;
+
+                } catch (NumberFormatException e) {System.out.println("Sadece sayi girebilirsiniz");}
+            }
+
+            return cikti;
+        }
+        //Double Degerler icin.
+        double nextDouble() {
+            boolean gecerlilik = false;
+            String girdi;
+            double cikti = 0;
+
+            while (!gecerlilik) {
+                girdi = scanner.nextLine();
+                try {
+                    cikti = Double.parseDouble(girdi);
+                    gecerlilik = true;
+
+                } catch (NumberFormatException e) {System.out.println("Sadece sayi girebilirsiniz");}}
+
+            return cikti;
+        }
+
+/*Bu metod ile birlikte belirledigimiz kullanicinin sadece belirledigimiz
+aralikta ki tam sayilari girmesini sagliyoruz.*/
+int nextIntForNavigation(int taban, int azami) {
+         int girdi= nextInt();
+while (girdi >= taban&&girdi <= azami) {
+    System.out.println("Sadece["+taban+" - "+azami+"]araliginda bir sayi girebilirsiniz.");
+    girdi = nextInt();
+}
+
+    return girdi;
+}
+     }
 }
