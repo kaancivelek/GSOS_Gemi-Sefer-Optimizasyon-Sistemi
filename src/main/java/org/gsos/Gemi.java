ef75fe46868434a127451f1b9865c100ab84190c
Kaan Civelek <businesskaancivelek@gmail.com>
2024-12-28 22:11:37 +0300

    gemi,veri yönetimi,hata yakalama sınıflarında okunabilirlik arttıracak şeyler yapıldı. bkz:hizalamalar boşluklar yeniden düzenlendi. Yorum satırları eklendi. GuvenliScanner classının adı Hatayakalama olarak değiştirildi.

--- src/main/java/org/gsos/Gemi.java
+++ src/main/java/org/gsos/Gemi.java
@@ -3,31 +3,30 @@ package org.gsos;
 import java.util.Scanner;
 
 public abstract class Gemi {
-    GuvenliScanner gs = new GuvenliScanner();
+    // Kullanıcıdan giriş almak için Scanner nesneleri
+    HataYakalama gs = new HataYakalama();
     Scanner s = new Scanner(System.in);
+
+    // Gemiye ait özellikler
     String gemiAdi;
-     String yapiMalzeme;
-     double agirlik;
-     double hacim;
-     double azamiYuk;
-     double yukMik;
-     double hiz;
+    String yapiMalzeme;
+    double agirlik;
+    double hacim;
+    double azamiYuk;
+    double yukMik;
+    double azamiHiz;
 
-    public Gemi() {
+    // Varsayılan yapıcı metot
+    public Gemi() {}
 
-    }
-
-
+    // Alt sınıfların uygulaması gereken soyut metotlar
     public abstract void yukMikHesapla();
+    public abstract void gemiCalistir();
+    public abstract void gemiDurdur();
 
-    public abstract void  gemiCalistir();
-
-    public abstract void   gemiBilgisi();
-
-    public abstract void  gemiDurdur();
-
+    // Gemi motorunu temsil eden iç sınıf
     public class Motor {
-
+        // Motor özellikleri
         private double metalYorgunlugu;
         private double RPM;
         private double yakitTuketimi;
@@ -35,109 +34,94 @@ public abstract class Gemi {
         private String yakitKalitesi;
         private double yakitMik;
 
-
+        // Motor özelliklerini başlatan yapıcı metot
         public Motor(double metalYorgunlugu, double RPM, double yakitTuketimi, boolean bakimDurumu, String yakitKalitesi, double yakitMik) {
-
-
             this.metalYorgunlugu = metalYorgunlugu;
             this.RPM = RPM;
             this.yakitTuketimi = yakitTuketimi;
             this.bakimDurumu = bakimDurumu;
             this.yakitKalitesi = yakitKalitesi;
             this.yakitMik = yakitMik;
+
+            // Yakıt bilgilerini ekrana yazdır
             yakitBilgisi();
-
         }
 
-        // Metotlar
+        // Bakım işlemi için metot
         public boolean bakim() {
-            // Bakım işlemleri
-            return true;
+            return true; // Bakım için varsayılan mantık
         }
 
-        public void  motorCalistir() {
-
+        // Motoru çalıştıran metot
+        public void motorCalistir() {
+            // Motor çalıştırma mantığı placeholder
         }
 
-        public void  motorDurdur() {
-
+        // Motoru durduran metot
+        public void motorDurdur() {
+            // Motor durdurma mantığı placeholder
         }
 
+        // Yakıt bilgilerini ekrana yazdıran metot
         public void yakitBilgisi() {
-            System.out.println("Yakıt bilgisi:\n Kalite = " + yakitKalitesi + ", Miktar = " + yakitMik);
+            System.out.println("Yakit bilgisi:\n Kalite = " + yakitKalitesi + ", Miktar = " + yakitMik);
         }
     }
 }
 
 class YukGemisi extends Gemi {
+    // Yuk gemisi özelliklerini başlatan yapıcı metot
+    YukGemisi() {
+        super(); // Üst sınıfın yapıcı metodunu çağır
 
+        // Kullanıcıdan gemi detaylarını al
+        System.out.println("Gemi adini giriniz:");
+        String gemiAdi = s.nextLine();
 
-
-
-    @Override
-    public void yukMikHesapla() {
-
-    }
-
-    @Override
-    public void gemiCalistir() {
-
-    }
-
-    @Override
-    public void gemiBilgisi() {
-        System.out.println("---GEMİ BİLGİSİ---");
-        System.out.println(
-                "adi:" +gemiAdi+"\n"+
-                "yapi malzemesi:" +yapiMalzeme+"\n"+
-                "agirligi:" +agirlik+"ton"+"\n"+
-                "hacmi" +hacim+"metreküp"+"\n"+
-                "azami yuk siniri:" +azamiYuk+"ton"+"\n"+
-                "yuk miktari:" +yukMik+"ton"+"\n"+
-                "hizi:" +hiz+"km"
-                );
-    }
-
-    @Override
-    public void gemiDurdur() {
-
-    }
-
-    YukGemisi()
-    {super();
-
-
-
-
-        System.out.println("değerleri giriniz:");
-        System.out.println("gemi adini giriniz:");
-        String gemiAdi= s.nextLine();
         System.out.println("Yapi malzemesi:");
-        String  yapiMalzeme= s.nextLine();;
+        String yapiMalzeme = s.nextLine();
+
         System.out.println("Agirlik giriniz:");
         double agirlik = gs.nextDouble();
+
         System.out.println("Hacim giriniz:");
         double hacim = gs.nextDouble();
-        System.out.println("azamiyuk giriniz:");
-        double azamiYuk= gs.nextDouble();
+
+        System.out.println("Azami yuk giriniz:");
+        double azamiYuk = gs.nextDouble();
+
         System.out.println("Yuk miktari giriniz:");
-        double yukMik=gs.nextDouble();
+        double yukMik = gs.nextDouble();
+
         System.out.println("Hiz giriniz:");
         double hiz = gs.nextDouble();
 
+        // Degerleri özelliklere ata
+        this.gemiAdi = gemiAdi;
         this.yapiMalzeme = yapiMalzeme;
         this.agirlik = agirlik;
         this.hacim = hacim;
         this.azamiYuk = azamiYuk;
         this.yukMik = yukMik;
-        this.hiz = hiz;
-        this.gemiAdi=gemiAdi;
+        this.azamiHiz = hiz;
 
-
-      /*  gemiBilgisi();*/
-        Motor m= new Motor(0,25,24,true,"a kalite",2500);
-        }
-
-
+        // Motoru baslat
+        Motor m = new Motor(0, 25, 24, true, "A kalite", 2500);
     }
 
+    // Soyut metotların uygulamaları
+    @Override
+    public void yukMikHesapla() {
+        // Yuk miktarini hesaplama mantigi placeholder
+    }
+
+    @Override
+    public void gemiCalistir() {
+        // Gemiyi calistirma mantigi placeholder
+    }
+
+    @Override
+    public void gemiDurdur() {
+        // Gemiyi durdurma mantigi placeholder
+    }
+}
--- src/main/java/org/gsos/VeriYonetimi.java
+++ src/main/java/org/gsos/VeriYonetimi.java
@@ -2,105 +2,106 @@ package org.gsos;
 import java.util.ArrayList;
 
 
+// T verisiyle calisan soyut sinif. Tum veri yonetim islemleri icin temel gorevleri saglar.
 abstract class VeriYonetimi<T> {
-    ArrayList<T> veriler;
+    public ArrayList<T> veriler;
 
-    public VeriYonetimi() {this.veriler = new ArrayList<>();}
+    public VeriYonetimi() {
+        this.veriler = new ArrayList<>();
+    }
 
-    public void ekle(T kayit) {this.veriler.add(kayit);}
+    // Yeni kayit ekler.
+    public void ekle(T kayit) {
+        this.veriler.add(kayit);
+    }
 
+    // Alt siniflarin doldurmasi gereken listeleme metodu.
     public abstract void listele();
-
 }
 
+// Gemi verilerini listeleyen sinif.
 class GemiListesi extends VeriYonetimi<Gemi> {
     @Override
     public void listele() {
 
-//String format metodu kullanarak internette ASCII ART yaparak duzgun bir sekilde verilerimizi yazdirmayi hedefledim.
-            String boyutlandirma= "| %-15s | %-15s | %-10s | %-15s | %-15s | %-15s | %-10s |%n";
-            String cizgi = "+-----------------+-----------------+------------+-----------------+-----------------+-----------------+------------+";
+        // Tablo boyutlandirma ve cizgi formatlari.
+        String boyutlandirma = "| %-15s | %-15s | %-10s | %-15s | %-15s | %-15s | %-10s |%n";
+        String cizgi = "+-----------------+-----------------+------------+-----------------+-----------------+-----------------+------------+";
 
-//bu kısım tablo başlığı
-            System.out.println(cizgi);
-            System.out.printf(boyutlandirma, "Gemi Adi","Yapı Malzeme", "Ağırlık(Ton)", "Hacim(Gros Tonaj)", "Azami Yük(Ton)", "Yük Mik(Ton)", "Azami Hız(Knot)");
-            System.out.println(cizgi);
-//bu kısım tablo başlığı
+        // Tablo basligi yazdir.
+        System.out.println(cizgi);
+        System.out.printf(boyutlandirma, "Gemi Adi", "Yapı Malzeme", "Ağırlık(Ton)", "Hacim(Gros Tonaj)", "Azami Yük(Ton)", "Yük Mik(Ton)", "Azami Hız(Knot)");
+        System.out.println(cizgi);
 
-            // bu kısım tablo öğelerini ekrana yazdırmayı içeriyor.
-            for (Gemi gemi : veriler) {
-               System.out.printf(boyutlandirma,
-                        gemi.gemiAdi,
-                        gemi.yapiMalzeme,
-                        String.format("%.2f", gemi.agirlik),
-                        String.format("%.2f", gemi.hacim),
-                        String.format("%.2f", gemi.azamiYuk),
-                        String.format("%.2f", gemi.yukMik),
-                        String.format("%.2f", gemi.hiz));
-            }System.out.println(cizgi);
+        // Gemi verilerini ekrana yazdir.
+        for (Gemi gemi : veriler) {
+            System.out.printf(boyutlandirma,
+                    gemi.gemiAdi,
+                    gemi.yapiMalzeme,
+                    String.format("%.2f", gemi.agirlik),
+                    String.format("%.2f", gemi.hacim),
+                    String.format("%.2f", gemi.azamiYuk),
+                    String.format("%.2f", gemi.yukMik),
+                    String.format("%.2f", gemi.azamiHiz));
         }
-
+        System.out.println(cizgi);
     }
+}
 
-    class SeferKayitlari extends VeriYonetimi<Sefer> {
-        @Override
-        public void listele() {
+// Sefer kayitlarini listeleyen sinif.
+class SeferKayitlari extends VeriYonetimi<Sefer> {
+    @Override
+    public void listele() {
 
+        // Tablo boyutlandirma ve cizgi formatlari.
+        String boyutlandirma = "| %-15s | %-15s | %-20s | %-15s | %-20s | %-15s | %-15s |%n";
+        String cizgi = "+-----------------+-----------------+----------------------+-----------------+----------------------+-----------------+-----------------+";
 
-//String format metodu kullanarak internette ASCII ART yaparak duzgun bir sekilde verilerimizi yazdirmayi hedefledim.
-            String boyutlandirma= "| %-15s | %-15s | %-20s | %-15s | %-20s | %-15s | %-15s |%n";
-            String cizgi = "+-----------------+-----------------+----------------------+-----------------+----------------------+-----------------+-----------------+";
+        // Tablo basligi yazdir.
+        System.out.println(cizgi);
+        System.out.printf(boyutlandirma, "Sefer Numarasi", "Geminin Adi", "Sefer Guzergahi(A-B)", "Sefer Suresi(Saat)", "Tehlike Duzeyi(%)", "Gelir(TL)", "Gider(TL)");
+        System.out.println(cizgi);
 
-//bu kısım tablo başlığı
+        // Sefer verilerini ekrana yazdir.
+        for (Sefer sefer : veriler) {
+             System.out.printf(boyutlandirma,
+                    sefer.seferNumarasi,
+                    sefer.gemiAdi,
+                    String.format("%.2f", sefer.guzergah),
+                    String.format("%.2f", sefer.tamamlamaSuresi),
+                    String.format("%.2f", sefer.tehlikeDuzeyi),
+                    String.format("%.2f", sefer.toplamGelir),
+                    String.format("%.2f", sefer.toplamGider));
             System.out.println(cizgi);
-            System.out.printf(boyutlandirma, "Sefer Numarasi","Geminin Adi","Sefer Guzergahi(A-B)", "Sefer Suresi(Saat)", "Tehlike Duzeyi(%)", "Gelir(TL)", "Gider(TL)");
-            System.out.println(cizgi);
-//bu kısım tablo başlığı
-
-            // bu kısım tablo öğelerini ekrana yazdırmayı içeriyor.
-            for (Sefer sefer : veriler) {
-               /* System.out.printf(boyutlandirma,
-                        sefer.seferNumarasi,
-                         sefer.gemiAdi,
-                        String.format("%.2f", sefer.guzergah),
-                        String.format("%.2f", sefer.tamamlamaSuresi),
-                        String.format("%.2f", sefer.tehlikeDuzeyi),
-                        String.format("%.2f", sefer.toplamGelir),
-                        String.format("%.2f", sefer.toplamGider));
-            }System.out.println(cizgi);*/
-        }
-
-        }
-
-    }
-
-    class SeferDetaylari extends VeriYonetimi<Sefer> {
-        @Override
-        public void listele() {
-            //String format metodu kullanarak internette ASCII ART yaparak duzgun bir sekilde verilerimizi yazdirmayi hedefledim.
-            String boyutlandirma= "| %-20s | %-25s | %-15s | %-20s | %-10s | %-15s | %-15s | %-15s | %-15s |%-15s |%n";
-            String cizgi = "+----------------------+--------------------------+-----------------+-----------------------+-------------------+-----------------+-----------------+-----------------+-----------------+-----------------+";
-
-//bu kısım tablo başlığı
-            //System.out.println(sefer.seferNumarasi+" NUMARALI SEFERIN DETAYLARI");
-            System.out.println(cizgi);
-            System.out.printf(boyutlandirma, "Seferdeki sure(Saat)","Kat edilen Mesafe(Deniz Mili)","Yakit Tuketimi(LT/sa)","Metal Yorgunlugu","Hiz(knot)","Ruzgar Hizi(km/sa)","Ruzgar Yonu", "Akinti Hızı(cm/s)", "Akinti Yonu");
-            System.out.println(cizgi);
-//bu kısım tablo başlığı
-
-            // bu kısım tablo öğelerini ekrana yazdırmayı içeriyor.
-            for (Sefer sefer : veriler) {
-               /* System.out.printf(
-               boyutlandirma,
-                        sefer.Numarasi,
-                         sefer.gemiAdi,
-                        String.format("%.2f", sefer.guzergah),
-                        String.format("%.2f", sefer.tamamlamaSuresi),
-                        String.format("%.2f", sefer.tehlikeDuzeyi),
-                        String.format("%.2f", sefer.toplamGelir),
-                        String.format("%.2f", sefer.toplamGider));
-            }System.out.println(cizgi);*/
-
         }
     }
+}
+
+// Sefer detaylarini listeleyen sinif.
+class SeferDetaylari extends VeriYonetimi<Sefer> {
+    @Override
+    public void listele() {
+
+        // Tablo boyutlandirma ve cizgi formatlari.
+        String boyutlandirma = "| %-20s | %-25s | %-15s | %-20s | %-10s | %-15s | %-15s | %-15s | %-15s |%n";
+        String cizgi = "+----------------------+--------------------------+-----------------+-----------------------+-------------------+-----------------+-----------------+-----------------+-----------------+";
+
+        // Tablo basligi yazdir.
+        System.out.println(cizgi);
+        System.out.printf(boyutlandirma, "Seferdeki sure(Saat)", "Kat edilen Mesafe(Deniz Mili)", "Yakit Tuketimi(LT/sa)", "Metal Yorgunlugu", "Hiz(knot)", "Ruzgar Hizi(km/sa)", "Ruzgar Yonu", "Akinti Hızı(cm/s)", "Akinti Yonu");
+        System.out.println(cizgi);
+
+        // Sefer detaylarini ekrana yazdir.
+        for (Sefer sefer : veriler) {
+            System.out.printf(boyutlandirma,
+                    sefer.Numarasi,
+                    sefer.gemiAdi,
+                    String.format("%.2f", sefer.guzergah),
+                    String.format("%.2f", sefer.tamamlamaSuresi),
+                    String.format("%.2f", sefer.tehlikeDuzeyi),
+                    String.format("%.2f", sefer.toplamGelir),
+                    String.format("%.2f", sefer.toplamGider));
+            System.out.println(cizgi);
+        }
     }
+}
